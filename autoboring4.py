da5d9fea3dc6faef650424e78740752950e0d079
jpeterseni35 <49698658+jpeterseni35@users.noreply.github.com>
2019-05-10 17:08:35 -0500

    Adding py files
    yum py

---
+++ autoboring4.py
@@ -0,0 +1,24 @@
+myAge = 26
+myAge < 30
+
+
+False and False
+
+
+True or True
+
+not False or False
+
+
+myAge = 26
+myPet = 'Bug'
+
+myAge == 26 and myPet == 'Bug'
+
+
+name = 'Alice'
+if name == 'Alice':
+    print('Hi, Alice')
+print('Done')
+
+test
