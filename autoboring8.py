3a4bd645a0e1a634a7d142e835b6cd286197df60
jpeterseni35 <jpeterseni35@gmail.com>
2019-07-28 17:05:49 -0500

    newFile

---
+++ autoboring8.py
@@ -0,0 +1,18 @@
+import pyperclip
+import sys
+import random
+import sys
+import os
+import math
+# random.randint(1, 10)
+
+from random import *
+randint(1, 10)
+
+print('Hello')
+sys.exit()
+print('Goodbye')
+
+
+pyperclip.copy('Hello world!')
+pyperclip.paste()
