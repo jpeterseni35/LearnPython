a7aa8501a1c96adda5f9da34a60dc9e4db2a8be5
jpeterseni35 <japeters@ruan.com>
2020-01-24 16:28:22 -0600

    Autoboring 13 lesson in progress

---
+++ autoboring13.py
@@ -0,0 +1,24 @@
+spam = ['cat', 'bat', 'rat', 'elephant']
+
+
+print(spam[2])
+
+spam = [['cat', 'bat'], [10, 20, 30, 40, 50]]
+
+print(spam[1][2])
+
+# slice
+spam[1][0:2]
+
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+
+# index
+spam[-2]
+
+
+spam[3] = 'Door Knob'
+spam[3]
+print(spam)
+
+# I am at 6:48 in lesson 13
