c35975dac77fa58104a30f8a846c5603f9a2f462
jpeterseni35 <jpeterseni35@gmail.com>
2020-02-07 15:46:14 -0600

    autoboring13 completion

--- autoboring13.py
+++ autoboring13.py
@@ -1,3 +1,4 @@
+# list creation named spam
 spam = ['cat', 'bat', 'rat', 'elephant']
 
 
@@ -5,7 +6,7 @@ print(spam[2])
 
 spam = [['cat', 'bat'], [10, 20, 30, 40, 50]]
 
-print(spam[1][2])
+print(spam[1][4])
 
 # slice
 spam[1][0:2]
@@ -21,4 +22,20 @@ spam[3] = 'Door Knob'
 spam[3]
 print(spam)
 
-# I am at 6:48 in lesson 13
+spam[0:3] = ['CAT', 'DOG', 'MOUSE']
+spam
+
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+del spam[2]
+spam
+
+len([1, 2, 3])
+
+[1, 2, 3]*3
+
+# converts string/values into a list format
+list('Fifty Five')
+
+
+'howdy' in ['hello', 'hi', 'howdy', 'heyas']
