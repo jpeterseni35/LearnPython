41b55c2963e23e381f7f9306e9c5e3d5d6b453b7
jpeterseni35 <jpeterseni35@gmail.com>
2021-02-26 10:59:59 -0600

    consolidating git

---
+++ iflearn.py
@@ -0,0 +1,34 @@
+spam = True
+
+spam
+
+
+42 == 42
+
+42 == 'Hello'
+
+42 == 41
+
+2 != 3
+
+42 >= 42
+
+myAge = 28
+myAge < 30
+
+print ('What is your age?')
+myAge = input()
+int(myAge) > 30
+
+False and True
+
+myAge = 27
+myPet = 'Bug'
+myAge > 30 and myPet == 'Bug'
+
+
+
+name = 'Bob'
+if name == 'Alice':
+    print('Hi Alice')
+print('Done')
